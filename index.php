5742319f680f360c085b54be0f5ae9f1ff80f2ff
emil georgi <emil376g@edu.campusvejle.dk>
2017-09-13 09:00:51 +0200

    eksamen

--- index.php
+++ index.php
@@ -19,76 +19,78 @@
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Karla|Lato|Oswald" rel="stylesheet">
 </head>
 
 <body>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <div class="container-fluid bg-faded">
+    <div class="container-fluid bg-faded p-0">
 
-    <header class="header">
-        <div class="container m-auto">
-                <div class="row col-12">
+        <header class="header">
+            <div class="container m-auto">
+                <div class="row col-12 pt-2">
                     <div class="header-top-left">
                         <button class="header-top-left-button d-flex" disabled="disabled"><img src="img/ikon.png" alt="Danmark's flag" class="header-top-left-img"><p class="header-top-left-p-icon-span">dansk</p></button>
                         <button class="header-top-left-button" disabled="disabled"><p class="header-top-left-p-DKK">DKK</p></button>
                     </div>
-                    <div class="header-top-right ml-auto">
+                    <div class="header-top-right ml-md-auto">
                         <form action="search.php" class="d-flex" method="get">
                             <input type="text" class="search" name="search" placeholder="søgning i produkter">
                             <input type="submit" class="header-top-right-submit" value="søg" name="submit">
                         </form>
                     </div>
                 </div>
-        </div>
-        <hr>
-        <div class="container m-auto">
-            <div class="row col-12">
-                <img src="img/homeIcon.png" alt="tilbage til start menu" href="index.php"><a href="index.php"></a>
             </div>
-        </div>
-        <hr>
-        <div class="container m-auto">
-            <div class="row">
-                <nav class="navbar navbar-toggleable-md navbar-light ">
-                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
-                        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <hr>
+            <div class="container m-auto">
+                <div class="row col-12">
+                    <a href="index.php">
+                <img src="img/homeIcon.png" alt="tilbage til start menu"></a>
+                </div>
+            </div>
+            <hr>
+            <div class="container m-auto">
+                <div class="row col-12">
+                    <nav class="navbar navbar-toggleable-md navbar-light col-sm-2 col-md-8">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
+                            aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Forside <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Produkter</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Nyheder</a>
-                            </li>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                            <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                                <li class="nav-item active">
+                                    <a class="nav-link active" href="#">Forside <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Produkter</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Nyheder</a>
+                                </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Handelsbetingelser</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Om os</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">login</a>
-                            </li>
-                        </ul>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Handelsbetingelser</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Om os</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div class="header-right-varer  ml-auto mt-auto">
+                        <form action="varer.php" class="d-flex" method="get">
+                            <input type="text" name="varer" class="header-right-varer-cart" disabled="disabled" value="din indkøbskurv er tom">
+                            <button class="header-right-varer-button" disabled="disabled"><i class="fa fa-shopping-cart" aria-hidden="true" style="font"></i></button>
+                        </form>
                     </div>
-                </nav>
-                <div class="header-right-varer  ml-auto mt-auto">
-                    <form action="varer.php" class="d-flex" method="get">
-                        <input type="text" name="varer" class="header-right-varer-cart" value="din indkøbskurv er tom">
-                        <button class="header-right-varer-button" disabled="disabled"><i class="fa fa-shopping-cart" aria-hidden="true" style="font"></i></button>
-                    </form>
                 </div>
             </div>
-        </div>
         </header>
         <hr>
         <div class="container m-auto">
@@ -121,59 +123,87 @@
                 </div>
         </div>
         <hr>
-        <div class="container m-auto">
-            <h1 class="main-title col-8 m-auto">Fancyclothes.DK - tøj, kvlitet, simpelt</h1>
+        <div class="container m-auto p-0">
+            <h1 class="main-title col-md-9 ml-2 col-sm-12 m-auto">Fancyclothes.DK - tøj, kvlitet, simpelt</h1>
         </div>
         <hr>
-        <div class="container">
-            <aside class="aside-search col-2">
-            <h3 class="aside-search-categori">Kategorier:</h3>
-                <form action="search.php" class="d-flex flex-column" method="get">
-                    <input type="button" value="jakker" class="aside-categori-button bg-faded" name="jakker">
-                    <input type="button" value="bukser" class="aside-categori-button bg-faded" name="bukser">
-                    <input type="button" value="skjorter" class="aside-categori-button bg-faded" name="skjorter">
-                    <input type="button" value="strik" class="aside-categori-button bg-faded" name="strik">
-                    <input type="button" value="t-shirts & tank tops" class="aside-categori-button bg-faded" name="t-shirts">
-                    <input type="button" value="tasker" class="aside-categori-button bg-faded" name="tasker">
-                </form>
-            </aside>
-            <aside class="login col-3 mt-2">
-            <h3 class="nyhedsbrev">tilmeld nyhedsbrev</h3>
-            <form action="nyhedsbrev d-flex flex-column" method="post">
-            <input type="text" name="username" placeholder="navn">
-            <input type="text" name="email" placeholder="email">
-            </form>
-            </aside>
+        <div class="container d-flex p-0">
+            <div class="col-md-3 col-sm-12 m-0">
+                <aside class="aside-search">
+                    <h3 class="aside-search-categori">Kategorier:</h3>
+                    <form action="search.php" class="d-flex p-0 flex-column col-12" method="get">
+                        <input type="button" value="jakker" class="aside-categori-button bg-faded" name="jakker">
+                        <input type="button" value="bukser" class="aside-categori-button bg-faded" name="bukser">
+                        <input type="button" value="skjorter" class="aside-categori-button bg-faded" name="skjorter">
+                        <input type="button" value="strik" class="aside-categori-button bg-faded" name="strik">
+                        <input type="button" value="t-shirts & tank tops" class="aside-categori-button bg-faded" name="t-shirts">
+                        <input type="button" value="tasker" class="aside-categori-button bg-faded" name="tasker">
+                    </form>
+                </aside>
+                <aside class="aside-nyhedsbrev d-flex flex-column col-12  mt-2">
+                    <h3 class="h3-nyhedsbrev">tilmeld nyhedsbrev</h3>
+                    <form action="nyhedsbrev.php" class="form-nyhedsbrev" method="post">
+                        <div class="div-nyhedsbrev justify-content-center py-3 d-flex col-12 flex-column">
+                            <input type="text" name="username" placeholder="navn">
+                            <input type="text" name="email" placeholder="email">
+                        </div>
+                        <input type="submit" class="col-4 mt-2 ml-3" value="submit">
+                    </form>
+                </aside>
+            </div>
+            <div class="container">
+                <h5 class="toj">inspiration</h3>
+                    <hr>
+                    <div class="row flex-grow col-md-12 justify-content-between p-0 m-0">
+                        <div class="herretoj flex-1 mr-1">
+                            <h3>herretøj</h3><br><button class="toj" disabled="disabled">læs mere</button></div>
+                        <div class="kvindetoj flex-1  ml-1">
+                            <h3>dametøj</h3><br><button class="toj" disabled="disabled">læs mere</button></div>
+                    </div>
+            </div>
+</div>
+            <div class="container">
+                <article class="content">
+
+                </article>
+            </div>
+            </main>
+            <div class="container-fluid p-0">
+            <footer class="footer col-12 d-flex justify-content-between">
+                <p class="footer-p"><strong>Fancyclothes.dk</strong></p>
+                <p class="footer-p">Skrædderstien 7</p>
+                <p class="footer-p">4321 Fredensvang</p>
+                <p class="footer-p">Email: info@fancyness@gmail.com</p>
+                <p class="footer-p">Sitemap</p>
+            </footer>
+            </div>
         </div>
-        </main>
 
-    </div>
+        <script>
+            window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')
 
-    <script>
-        window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')
+        </script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
 
-    </script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function (b, o, i, l, e, r) {
+                b.GoogleAnalyticsObject = l;
+                b[l] || (b[l] =
+                    function () {
+                        (b[l].q = b[l].q || []).push(arguments)
+                    });
+                b[l].l = +new Date;
+                e = o.createElement(i);
+                r = o.getElementsByTagName(i)[0];
+                e.src = 'https://www.google-analytics.com/analytics.js';
+                r.parentNode.insertBefore(e, r)
+            }(window, document, 'script', 'ga'));
+            ga('create', 'UA-XXXXX-X', 'auto');
+            ga('send', 'pageview');
 
-    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script>
-        (function (b, o, i, l, e, r) {
-            b.GoogleAnalyticsObject = l;
-            b[l] || (b[l] =
-                function () {
-                    (b[l].q = b[l].q || []).push(arguments)
-                });
-            b[l].l = +new Date;
-            e = o.createElement(i);
-            r = o.getElementsByTagName(i)[0];
-            e.src = 'https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e, r)
-        }(window, document, 'script', 'ga'));
-        ga('create', 'UA-XXXXX-X', 'auto');
-        ga('send', 'pageview');
-
-    </script>
+        </script>
 </body>
 
 </html>
